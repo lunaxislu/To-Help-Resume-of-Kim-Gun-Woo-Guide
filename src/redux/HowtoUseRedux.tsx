d73f2e8a8244df9b35175a0a3c7ef6ed202f3dac
Rock Kyun <mjnn100801@naver.com>
2024-01-05 13:29:16 +0900

    프로젝트 초기 - redux 보일러플레이트 적용

---
+++ src/redux/HowtoUseRedux.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useAppDispatch, useAppSelector } from './reduxHooks/reduxBase';
+
+const HowtoUseRedux = () => {
+  // count 라는 슬라이스를 가져올 때 /reduxHooks/reduxBas에 만들어둔 커스텀 훅(useAppSelector)로 가져온다.
+  const count = useAppSelector((state) => state.counter.value);
+
+  // countSlice의 reducer를 사용하고 싶으면 useAppdisPatch를 가져와서 사용한다
+  const dispatch = useAppDispatch();
+  return <div>reduxExample</div>;
+};
+
+export default HowtoUseRedux;
---
+++ src/redux/modules/example.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store/store';
+
+// slice의 타입 정의
+interface CounterState {
+  value: number;
+}
+
+// slice 초기값 정의
+const initialState: CounterState = {
+  value: 0
+};
+
+// reducer
+export const counterSlice = createSlice({
+  name: 'counter',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    // reducer의 인자로 action이 들어간다면 action의 타입을 정의하여 함수 선언
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    }
+  }
+});
+
+// 생성한 reducer export
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// 컴포넌트에서 countSlice를 가져오고 싶을 때 사용 할 Hook을 export
+export const selectCount = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer;
