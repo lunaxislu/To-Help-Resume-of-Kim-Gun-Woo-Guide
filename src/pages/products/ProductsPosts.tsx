08667a7406188c2558ecaf2e0a0c582141841420
Liabinn <liabin124@gmail.com>
2024-01-12 02:08:44 +0900

    [ADD] products posts page

--- src/pages/products/ProductsPosts.tsx
+++ src/pages/products/ProductsPosts.tsx
@@ -5,12 +5,16 @@ import ProductsPostingButton from '../../components/prducts/ProductsPosts/Produc
 
 const ProductsPosts = () => {
   return (
-    <div>
-      <ProductsPostingButton />
-      <div>
+    <div style={{padding: '20px'}}>
+      <div style={{display: 'flex', flexDirection: 'row', lineHeight: '200%', marginBottom: '20px'}}>
+        <h1 style={{fontSize: '30px', fontWeight: 'bold'}}>기본 정보</h1>
+        <p style={{marginLeft: '10px'}}>*필수항목</p>
+      </div>
+      <div style={{marginBottom: '50px'}}>
         <ProductsImage />
         <ProductsExplanation />
       </div>
+        <ProductsPostingButton />
     </div>
   )
 }
