cb682651dc2a0b484b42d83d10de933fb7b0596d
Liabinn <liabin124@gmail.com>
2024-01-12 02:05:43 +0900

    [ADD] products list page 추가 (supabase data read) - image storage not yet

--- src/components/prducts/ProductsList/ProductsTags.tsx
+++ src/components/prducts/ProductsList/ProductsTags.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const ProductsTags = () => {
+  
+  const major = ['회화', '조소', '판화', '금속공예', '도예', '유리공예', '목공예', '섬유공예', '기타']
+
   return (
-    <div>ProductsTags</div>
+    <div style={{display: 'flex', flexDirection: 'row', listStyle: 'none', gap: '10px', marginBottom: '20px'}}>
+      {major.map(major => 
+        <li key={major} style={{width: '100px', backgroundColor: 'lightblue', textAlign: 'center', border: '1px solid skyblue'}}>{major}</li>
+      )}
+    </div>
   )
 }
 
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -4,7 +4,8 @@ import ProductList from '../../components/prducts/ProductsList/ProductList'
 
 const Products = () => {
   return (
-    <div>
+    <div style={{padding: '20px'}}>
+      <h2 style={{marginBottom: '20px', fontSize: '30px', fontWeight: 'bold'}}>122,234개의 상품이 거래되고 있어요</h2>
       <ProductsTags />
       <ProductList />
     </div>
