1e6cb0e7bf120000ddb1408b2aa6ddefefae0c00
hyewon <gpdnjs154@navr.com>
2024-01-11 19:43:15 +0900

    텍스트 에디터 적용중/ merge test

--- src/pages/community/WritePost.tsx
+++ src/pages/community/WritePost.tsx
@@ -1,29 +1,43 @@
+import { EditorState, convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 import { useState } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import { supabase } from './supabaseClient';
+
 const WritePost = () => {
   const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState(EditorState.createEmpty());
   const [category, setCategory] = useState('말머리 선택');
   const navigate = useNavigate();
+
   const addPost = async () => {
     try {
-      const { data, error } = await supabase
-        .from('community')
-        .insert([
-          {
-            title,
-            content,
-            category,
-            post_user: 'sweetPotato',
-            nickname: 'goguma'
-          }
-        ])
-        .select();
+      const contentState = content.getCurrentContent();
+      const rawContentState = convertToRaw(contentState);
+      const htmlContent = draftToHtml(rawContentState);
+
+      const { data, error } = await supabase.from('community').insert([
+        {
+          title,
+          content: htmlContent,
+          category,
+          post_user: 'sweetPotato',
+          nickname: 'goguma'
+        }
+      ]);
+
       if (error) throw error;
       navigate('/community');
-    } catch (error) {}
+    } catch (error) {
+      console.error('Error adding post:', error);
+    }
+  };
+
+  const handleEditorChange = (newState: EditorState) => {
+    setContent(newState);
   };
 
   return (
@@ -36,7 +50,7 @@ const WritePost = () => {
             setCategory(e.target.value);
           }}
         >
-          <option disabled hidden selected>
+          <option disabled hidden>
             말머리 선택
           </option>
           <option>꿀팁</option>
@@ -51,13 +65,14 @@ const WritePost = () => {
         />
       </div>
 
-      <textarea
-        value={content}
-        onChange={(e) => {
-          setContent(e.target.value);
-        }}
-      ></textarea>
+      <Editor editorState={content} onEditorStateChange={handleEditorChange} />
       <button onClick={() => addPost()}>등록하기</button>
+      {/* <button onClick={() => console.log(convertContentToHtml())}>
+        Convert to HTML
+      </button>
+      <button onClick={() => convertHtmlToContent()}>
+        Convert HTML to Content
+      </button> */}
     </Container>
   );
 };
