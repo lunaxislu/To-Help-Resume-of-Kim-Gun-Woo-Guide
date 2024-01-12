3dd357cbf21dbb202990f5bbae1d0ae2ac3b7c30
jihyun-j <jihyun9142@gmail.com>
2024-01-12 17:09:29 +0900

    [update] mypage branch 생성

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import { GlobalStyles } from '../styles/GlobalStyle';
 import Login from '../pages/login/Login';
+import MyPage from '../pages/mypage/MyPage';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
