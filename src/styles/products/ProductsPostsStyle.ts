98e5b3039089dcf58403c12bc0cdec8fc398b430
Liabinn <liabin124@gmail.com>
2024-01-24 03:32:47 +0900

    [STYLE] 데스크탑 CSS 마무리

--- src/styles/products/ProductsPostsStyle.ts
+++ src/styles/products/ProductsPostsStyle.ts
@@ -1,4 +1,4 @@
-import { VscCheck } from "react-icons/vsc";
+import { CgCloseO } from "react-icons/cg";
 import styled from "styled-components";
 
 // ProductsPosts
@@ -40,7 +40,27 @@ export const Hr = styled.hr`
 export const WrapperStyle = styled.div`
   display: flex;
   flex-direction: row;
-  margin-bottom: 5rem;
+  //margin-bottom: 5rem;
+`
+
+export const Validation = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 5rem;
+  padding: 1rem 0 0 19rem;
+`
+
+export const Validation2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 5rem;
+  padding: 1rem 0 0 19rem;
+  gap: 1rem;
+`
+
+export const ValidationText = styled.p`
+  color: red;
+  line-height: 1.8;
 `
 
 export const SemiTitle = styled.h2`
@@ -61,24 +81,42 @@ export const InputStyle = styled.input`
   width: 85.7rem;
   height: 5rem;
   background-color: var(--3-gray);
+  font-family: 'Pretendard-Medium';
   font-size: var(--fontSize-H5);
   border: none;
   border-radius: 0.7rem;
-  color: var(--8-gray);
+  color: var(--12-gray);
   padding-left: 2rem;
   &::placeholder {
     color: var(--5-gray);
   }
 `
 
-export const CountText = styled.span`
+export const ImgCount = styled.span`
   height: 100%;
   margin-left: 2rem;
   color: var(--7-gray);
   font-size: var(--fontSize-H6);
-  // 카운트 텍스트가 아래로 가지않음, 조건부 필요
-  line-height: 3;
-  vertical-align: bottom;
+  line-height: 2.7;
+`
+
+export const TitleCount = styled.span`
+  height: 100%;
+  margin-left: 2rem;
+  color: var(--7-gray);
+  font-size: var(--fontSize-H6);
+  line-height: 4;
+`
+
+export const CountWrapper = styled.div`
+  align-items: end;
+`
+
+export const ContentsCount = styled.span`
+  height: 100%;
+  margin-left: 2rem;
+  color: var(--7-gray);
+  font-size: var(--fontSize-H6);
 `
 
 export const InputWrapperStyle = styled.div`
@@ -140,6 +178,7 @@ export const AddressInputStyle = styled.input`
   height: 5rem;
   background-color: var(--3-gray);
   font-size: var(--fontSize-H6);
+  font-family: 'Pretendard-Medium';
   border: none;
   border-radius: 0.7rem;
   color: var(--8-gray);
@@ -152,6 +191,9 @@ export const AddressInputStyle = styled.input`
 export const GapStyle = styled.div`
   margin-bottom: 2.3rem;
 `
+export const GapStyle2 = styled.div`
+  margin-bottom: 5rem;
+`
 
 export const AddressBtn = styled.button`
   width: 10rem;
@@ -159,7 +201,9 @@ export const AddressBtn = styled.button`
   border: none;
   border-radius: 0.7rem;
   background-color: var(--3-gray);
+  font-family: 'Pretendard-Medium';
   color: var(--10-gray);
+  cursor: pointer;
   :hover {
     cursor: pointer;
   }
@@ -176,6 +220,7 @@ export const QualityWrapper = styled.div`
 
 export const QualityExplanation = styled.p`
   color: var(--6-gray);
+  padding: 0.6rem;
 `
 
 export const TextAreaStyle = styled.textarea`
@@ -183,11 +228,12 @@ export const TextAreaStyle = styled.textarea`
   min-height: 19.6rem;
   resize: none;
   background-color: var(--3-gray);
-  font-size: var(--fontSize-H6);
+  font-size: var(--fontSize-H5);
   border: none;
   border-radius: 0.7rem;
-  color: var(--8-gray);
+  color: var(--12-gray);
   padding: 2rem;
+  font-family: 'Pretendard-Medium';
   &::-moz-input-placeholder {
     line-height: 2;
   }
@@ -217,6 +263,29 @@ export const CaveatText = styled.p`
   margin-bottom: 2.7rem;
 `
 
+export const agreementCheckBoxStyle = styled.input`
+  appearance: none;
+  width: 2.8rem;
+  height: 2.8rem;
+  background-color: var(--5-gray);
+  border: none;
+  border-radius: 0.4rem;
+  position: relative;
+  cursor: pointer;
+  outline: none;
+  margin-left: 1rem;
+  /* 체크 표시 스타일 */
+  &:checked:before {
+    content: '✔';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.4rem;
+    color: var(--opc-100);
+  }
+`;
+
 export const BtnWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -232,6 +301,7 @@ export const WriteBtn = styled.button`
   height: 4.8rem;
   font-size: var(--fontSize-H4);
   font-weight: var(--fontWeight-bold);
+  font-family: 'Pretendard-Medium';
   &:hover {
     cursor: pointer;
   }
@@ -257,7 +327,8 @@ export const ImageCard = styled.div`
   border-radius: 0.5rem;
   width: 18.5rem;
   height: 18.5rem;
-  position: relative;
+  display: flex;
+  flex-direction: row-reverse;
   :hover {
     cursor: pointer;
   }
@@ -269,12 +340,49 @@ export const Image = styled.img`
   width: 100%;
   height: 100%;
   border-radius: 0.5rem;
-  position: relative;
 `
 
-export const ImageDeleteBtn = styled.button`
+export const ImageOrderWrapper = styled.div`
+  border: 0.15rem solid var(--opc-100);
+  width: 18.5rem;
+  height: 18.5rem;
+  border-radius: 0.5rem;
+  padding: 1.2rem 0 0 1.2rem;
   position: absolute;
 `
+
+export const ImageOrder = styled.div`
+  width: 4.9rem;
+  height: 1.9rem;
+  border-radius: 0.4rem;
+  background-color: var(--opc-80);
+  color: var(--1-gray);
+  text-align: center;
+  line-height: 1.9;
+  font-size: 1rem;
+  font-weight: var(--fontWeight-semiBold);
+  :hover {
+    cursor: pointer;
+  }
+`
+
+export const ImageDeleteBtn = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 100%;
+  position: absolute;
+  :hover {
+    cursor: pointer;
+  }
+`
+
+export const ImageDeleteIcon = styled(CgCloseO)`
+  width: 100%;
+  height: 100%;
+  color: var(--5-gray);
+  position: absolute;
+`;
+
 // 사진 안으로 들어가도록 하고 꾸미기
 
 export const UpLoadBox = styled.label`
@@ -284,11 +392,14 @@ export const UpLoadBox = styled.label`
   border: 0.1rem solid var(--opc-20);
   background-color: var(--opc-10);
   color: var(--opc-100);
+  font-size: 5rem;
   text-align: center;
-  line-height: 11;
+  line-height: 3.3;
+  position: relative;
     &:hover {
     cursor: pointer;
   }
 `
+
 // 첫번째 사진만 테두리가 생기도록 조건부 걸어주기
 // 대표사진 걸어놓기
