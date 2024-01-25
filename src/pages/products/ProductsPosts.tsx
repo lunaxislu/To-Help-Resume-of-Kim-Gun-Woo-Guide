4cac28a38f692fb76938cddb90c1345fd2af94b4
Liabinn <liabin124@gmail.com>
2024-01-25 10:54:46 +0900

    [UPDATE] 뒤로가기 버튼 navigate 추가

--- src/pages/products/ProductsPosts.tsx
+++ src/pages/products/ProductsPosts.tsx
@@ -1,15 +1,23 @@
+import { useNavigate } from 'react-router'
 import ProductsWriteForm from '../../components/prducts/ProductsPosts/ProductsWriteForm'
 import * as St from '../../styles/products/ProductsPostsStyle'
 
 const ProductsPosts = () => {
 
+  const navigate = useNavigate();
   // 뒤로가기
+  const handleOnClickBack = () => {
+    const confirm = window.confirm('작성하신 글이 모두 지워집니다. 그래도 페이지를 떠나시겠습니까?')
+    if (confirm) {
+      navigate(-1)
+    }
+  }
 
   return (
     <St.EntireContainer>
       <St.ContentsContainer>
         <St.TitleContainer>
-          <St.BackBtnBox>
+          <St.BackBtnBox onClick={handleOnClickBack}>
             <St.BackIcon/>
           </St.BackBtnBox>
           <St.Title>판매하기</St.Title>
