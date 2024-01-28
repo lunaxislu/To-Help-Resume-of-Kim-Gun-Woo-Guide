e9cc480502c3057f0e8dd8c56629c3ab44bb44f9
Liabinn <liabin124@gmail.com>
2024-01-28 16:42:39 +0900

    [STYLE] 카테고리에 데이터 없을때 보여줄 이미지 변경

--- src/pages/products/PostsNothing.tsx
+++ src/pages/products/PostsNothing.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import * as St from '../../styles/products/ProductsNothingStyle';
 
-const PostsNothing = () => {
+interface Props {
+  selectCategory: string[];
+}
+
+const PostsNothing = ({ selectCategory }: Props) => {
   return (
-    <div>
-      
-    </div>
+    <St.NothingContainer>
+      <St.NothingWrapper>
+        <St.NothingIcon>
+          <St.ExclamationMark/>
+        </St.NothingIcon>
+        <p><span>{selectCategory}</span>에 속한 물품이 아직 없어요</p>
+        <p>'판매하기'를 눌러 가장 먼저 판매를 시작해보세요!</p>
+      </St.NothingWrapper>
+    </St.NothingContainer>
   )
 }
 
