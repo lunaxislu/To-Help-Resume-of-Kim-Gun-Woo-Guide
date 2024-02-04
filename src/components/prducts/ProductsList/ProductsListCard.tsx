82db221d86f4fc121ba9b48e70937d4bf0dcc44b
Liabinn <liabin124@gmail.com>
2024-02-05 01:37:54 +0900

    [UPDATE] pagenation -> infinite scroll 변경

--- src/components/prducts/ProductsList/ProductsListCard.tsx
+++ src/components/prducts/ProductsList/ProductsListCard.tsx
@@ -1,86 +1,19 @@
-import * as St from '../../../styles/products/ProductsListStyle';
-import { useQuery } from 'react-query';
-import { PAGE_POST_NUMBER, fetchRangeProductsPosts } from '../productsQuery';
-import { useEffect, useState } from 'react';
-import Pagination from '../../../pages/products/Pagination';
 import PostsNothing from '../../../pages/products/PostsNothing';
 import ProductsCard from '../ProductsCard';
+import { ProductsPostsType } from '../ProductsType';
 
 interface Props {
+  posts: ProductsPostsType[]
   selectCategory: string[];
 }
 
-const ProductListCard = ({ selectCategory }: Props) => {
-
-  // Pagination을 위한 State
-  const [totalPages, setTotalPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [posts, setPosts] = useState<any>();
-
-  const fetchTest = async () => {
-    try {
-      const res = await fetchRangeProductsPosts(currentPage, selectCategory);
-      setPosts(res.data);
-
-      if (res.count) {
-        setTotalPages(Math.ceil(res.count / PAGE_POST_NUMBER));
-      }
-    } catch (error) {
-      // 페이지네이션 이후 다른 카테고리 에러 날 때
-      return;
-    }
-  };
-
-  const {
-    data: productsPosts,
-    isLoading,
-    isError
-  } = useQuery(
-    ['productsPosts', currentPage],
-    async () => {
-      await fetchTest(); // fetchTest가 끝날 때까지 기다립니다.
-      return fetchRangeProductsPosts(currentPage, selectCategory);
-    },
-    {
-      onSuccess: (data) => {
-        if (data.count) {
-          setTotalPages(Math.ceil(data.count / PAGE_POST_NUMBER));
-        }
-      }
-    }
-  );
-
-  useEffect(() => {
-    // selectCategory가 변경될 때마다 실행될 작업
-    fetchTest();
-  }, [selectCategory]);
-
-  useEffect(() => {
-    setCurrentPage(1); // 카테고리가 바뀔 때마다 첫 페이지로 리셋
-  }, [selectCategory]);
-
-  if (isLoading) {
-    return <St.LoadingStyle>Loading...</St.LoadingStyle>;
-  }
-
-  if (isError) {
-    return (
-      <St.LoadingStyle>
-        정보를 가져올 수 없습니다. 다시 시도해주세요.
-      </St.LoadingStyle>
-    );
-  }
+const ProductListCard = ({ posts, selectCategory }: Props) => {
 
   return (
     <div>
       {posts && posts.length > 0 ? (
         <>
           <ProductsCard posts={posts} />
-          <Pagination
-            totalPages={totalPages}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
         </>
       ) : (
         <PostsNothing selectCategory={selectCategory}/>
--- src/pages/products/ProductsList.tsx
+++ src/pages/products/ProductsList.tsx
@@ -1,11 +1,12 @@
-import { useQuery } from 'react-query';
-import { MouseEvent, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { MouseEvent, useEffect, useState } from 'react';
 import ProductListCard from '../../components/prducts/ProductsList/ProductsListCard';
-import ProductsSearchBar from '../../components/prducts/ProductsList/ProductsSearchBar';
 import ProductsSortBtn from '../../components/prducts/ProductsList/ProductsSortBtn';
 import * as St from '../../styles/products/ProductsListStyle';
 import { useNavigate } from 'react-router';
-import { getProductsCount } from '../../components/prducts/productsQuery';
+import { fetchProductsPosts } from '../../components/prducts/productsQuery';
+import { useInView } from 'react-intersection-observer';
+import { ProductsPostsType } from '../../components/prducts/ProductsType';
 
 const major = [
   '전체',
@@ -18,23 +19,40 @@ const major = [
 const ProductsList = () => {
   const navigate = useNavigate();
 
-  const [selectCategory, setSelectCategory] = useState<string[]>(['전체']);
-
-  const {
-    data: productsCount,
-    isLoading,
-    isError
-  } = useQuery(['productsCount'], getProductsCount, {
-    staleTime: 60000,
-    onSuccess: (productsCount) => {
-      // 여기서 selectCategory를 이용한 작업을 수행
-      console.log('Data fetched successfully', productsCount);
+  // 판매글쓰기 함수를 위한 id 체크
+  const [userId, setUserID] = useState('');
+  
+  useEffect(() => {
+    const storedUserId = localStorage.getItem('userId');
+    if (storedUserId) {
+      setUserID(storedUserId);
     }
+  }, []);
+  const [selectCategory, setSelectCategory] = useState<string[]>(['전체']);
+  
+  // intersection Observer API
+  const {ref, inView} = useInView();
+  
+  // 무한스크롤을 위한 전체 or 카테고리별 데이터 가져오기(count도 함께 가져옴)
+  const {data, isError, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage 
+  } = useInfiniteQuery(['productInfinite', selectCategory],
+  ({pageParam = 1}) => fetchProductsPosts(selectCategory, pageParam, 10),
+  {
+   getNextPageParam: (lastPage, pages) => {
+    if (lastPage.data.length < 10) return undefined;
+    return pages.length + 1;
+   }
   });
 
-  if (isLoading) {
-    return <St.LoadingStyle>Loading...</St.LoadingStyle>;
-  }
+  // inView가 true이고 다음페이지가 존재한다면 다음페이지 데이터 불러오기
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
+
+  // 데이터 안에 있는 찐 data를 하나의 배열로 만들어주기
+  const posts: ProductsPostsType[] = data?.pages.flatMap(page => page.data) || [];
 
   if (isError) {
     return (
@@ -48,13 +66,29 @@ const ProductsList = () => {
     const target = e.target as HTMLElement;
     setSelectCategory([target.innerText || '']);
   };
+  
+  // 로그아웃된 유저 판매글 쓰기 막아주기
+  const handleOnClickSellWriteBtn = () => {
+    if (!userId) {
+      const confirmLogin = window.confirm('판매하기는 로그인 후에 가능합니다. 로그인 하시겠습니까?');
+      if (confirmLogin) {
+        navigate('/login');
+      }
+      return;
+    }
+    navigate('/productsposts');
+  }
 
   return (
     <St.EntireContainer>
       <St.ContentsContainer>
         <St.TitleContainer>
-          <St.Title>{`${productsCount}개의 물품이 거래되고 있어요`}</St.Title>
-          <St.PostsWriteBtn onClick={() => navigate('/productsposts')}>
+          <St.Title>
+            {data?.pages[0].count ? 
+              `${data?.pages[0].count}개의 물품이 거래되고 있어요` : 
+              `거래되고 있는 ${selectCategory} 물품이 없어요`}
+          </St.Title>
+          <St.PostsWriteBtn onClick={handleOnClickSellWriteBtn}>
             <St.SellWriteIcon /> 판매하기
           </St.PostsWriteBtn>
         </St.TitleContainer>
@@ -74,11 +108,16 @@ const ProductsList = () => {
             </St.CategoryWrapper>
           </St.CategoryContainer>
           {/* <St.SearchBarContainer>
-          <ProductsSearchBar />
           <ProductsSortBtn />
         </St.SearchBarContainer> */}
         </St.BarContainer>
-        <ProductListCard selectCategory={selectCategory} />
+        {isLoading ? (
+          <St.LoadingStyle>스켈레톤 이미지 props는 몇장 보여줄 것인지 갯수</St.LoadingStyle>
+        ) : (
+          <ProductListCard posts={posts} selectCategory={selectCategory} />
+        )}
+        {hasNextPage && !isFetchingNextPage && <div ref={ref}></div>}
+        {isFetchingNextPage && <div>스켈레톤 이미지 props는 몇장 보여줄 것인지 갯수</div>}
       </St.ContentsContainer>
     </St.EntireContainer>
   );
