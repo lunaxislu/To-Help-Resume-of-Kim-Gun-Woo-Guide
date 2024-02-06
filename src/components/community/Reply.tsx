fba0cfc2730665eff22afd8457485e388bc58221
hyewon <gpdnjs154@navr.com>
2024-02-06 22:27:21 +0900

    [css]간격조정

--- src/components/community/Reply.tsx
+++ src/components/community/Reply.tsx
@@ -1,33 +1,49 @@
 import React, { FormEvent, useEffect, useState } from 'react';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import 'react-quill/dist/quill.snow.css';
 import { supabase } from '../../api/supabase/supabaseClient';
 import * as St from '../../styles/community/CommentStyle';
 
 import { useNavigate } from 'react-router';
-import {
-  addCommentMutation,
-  deleteCommentMutation,
-  fetchComments,
-  updateCommentMutation
-} from '../../pages/community/api/ReplyQuery';
-import {
-  fetchDetailPost,
-  updatePostMutation
-} from '../../pages/community/api/commuQuery';
+import { fetchComments } from '../../pages/community/api/ReplyQuery';
+import { fetchDetailPost } from '../../pages/community/api/commuQuery';
 import {
   CommentProps,
   ProfileObject,
   ReplyObject
 } from '../../pages/community/api/model';
-import parseDate from '../../util/getDate';
-import ReplyContent from './ReplyContent';
+import {
+  useAddReplyMutation,
+  useDeleteReplyMutation,
+  useUpdatePostMutation,
+  useUpdateReplyMutation
+} from '../../pages/community/hook/useQuery';
+import ReplyForm from './ReplyForm';
+import ReplyList from './ReplyList';
 const Reply: React.FC<CommentProps> = ({
   userId,
   paramId,
   likes,
   postUserId
 }) => {
+  //꺄아악 state 무덤 react-hook-form 또는 custom-input만들자
+  const [isEdit, setIsEdit] = useState(false);
+  const [profile, setProfile] = useState<ProfileObject[]>([]);
+  const [comment, setComment] = useState('');
+  const [editComment, setEditComment] = useState('');
+  const [editedCommentIndex, setEditedCommentIndex] = useState<number | null>(
+    null
+  ); // 수정 중인 댓글의 인덱스
+  const [anon, setAnon] = useState(false);
+  const [secret, setSecret] = useState(false);
+  const [parentId, setParentId] = useState<number | null>(null);
+  const [parentUserId, setParentUserId] = useState<string | null>(null);
+
+  const [liked, setLiked] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [replyingToComment, setReplyingToComment] =
+    useState<ReplyObject | null>(null);
+
   //유저 데이터 가져오기
   const queryClient = useQueryClient();
   useEffect(() => {
@@ -38,14 +54,12 @@ const Reply: React.FC<CommentProps> = ({
             .from('user')
             .select('*')
             .eq('id', userId);
-          // console.log(user);
           if (error) {
             console.log(error);
           }
 
           if (user != null) {
             setProfile(user);
-            // console.log(user);
           }
         } catch (error: any) {
           console.log(error.message);
@@ -63,25 +77,6 @@ const Reply: React.FC<CommentProps> = ({
 
   const navigate = useNavigate();
 
-  //꺄아악 state 무덤 react-hook-form 또는 custom-input만들자
-  const [isEdit, setIsEdit] = useState(false);
-  const [comments, setComments] = useState<ReplyObject[]>([]);
-  const [profile, setProfile] = useState<ProfileObject[]>([]);
-  const [comment, setComment] = useState('');
-  const [editComment, setEditComment] = useState('');
-  const [editedCommentIndex, setEditedCommentIndex] = useState<number | null>(
-    null
-  ); // 수정 중인 댓글의 인덱스
-  const [anon, setAnon] = useState(false);
-  const [secret, setSecret] = useState(false);
-  const [parentId, setParentId] = useState<number | null>(null);
-  const [parentUserId, setParentUserId] = useState<string | null>(null);
-  const [isReply, setIsReply] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
-  const [replyingToComment, setReplyingToComment] =
-    useState<ReplyObject | null>(null);
-
   useEffect(() => {
     // 사용자가 이미 좋아요를 했는지 확인
     const userLiked = posts?.[0]?.likes_user?.includes(userId);
@@ -89,7 +84,7 @@ const Reply: React.FC<CommentProps> = ({
       setLiked(true);
     }
   }, [posts, userId]);
-
+  const updatePostMutation = useUpdatePostMutation();
   const toggleLike = async () => {
     const newLikedStatus = !liked;
     setLiked(newLikedStatus);
@@ -112,38 +107,26 @@ const Reply: React.FC<CommentProps> = ({
       },
       paramId
     };
-    upsertMutation.mutate(likesUpdate);
+    updatePostMutation.mutate(likesUpdate, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['posts', paramId]);
+        setAnon(false);
+        setComment('');
+        setSecret(false);
+        setParentId(null);
+        setParentUserId(null);
+      }
+    });
   };
-
-  const upsertMutation = useMutation(updatePostMutation, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(['posts', paramId]);
-      setAnon(false);
-      setComment('');
-      setSecret(false);
-      setParentId(null);
-      setParentUserId(null);
-    }
-  });
 
   const { data: commentUserInfo, isLoading: commentLoading } = useQuery(
     ['comments', paramId],
     () => fetchComments(paramId)
   );
-  // console.log(commentUserInfo);
-  //댓글 추가 쿼리 변환 완료
-  const addMutation = useMutation(addCommentMutation, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('comments');
-      setAnon(false);
-      setComment('');
-      setSecret(false);
-      setReplyingToComment(null);
-      setParentId(null);
-      setParentUserId(null);
-      setIsFocused(false);
-    }
-  });
+
+  const addMutation = useAddReplyMutation();
+  const updateMutation = useUpdateReplyMutation();
+  const deleteMutation = useDeleteReplyMutation();
 
   const addComment = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -156,7 +139,18 @@ const Reply: React.FC<CommentProps> = ({
       anon,
       secret
     };
-    addMutation.mutate(insertReply);
+    addMutation.mutate(insertReply, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['comments', paramId]);
+        setAnon(false);
+        setComment('');
+        setSecret(false);
+        setReplyingToComment(null);
+        setParentId(null);
+        setParentUserId(null);
+        setIsFocused(false);
+      }
+    });
   };
 
   const startEditComment = (commentId: number, currentContent: string) => {
@@ -164,43 +158,40 @@ const Reply: React.FC<CommentProps> = ({
     setEditedCommentIndex(commentId); // 수정 중인 댓글의 ID 설정
     setIsEdit(true);
   };
-  const updateMutation = useMutation(updateCommentMutation, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('comments');
-      setAnon(false);
-      setComment('');
-      setSecret(false);
-      setIsEdit(false);
-      setEditedCommentIndex(null);
-    }
-  });
+
   const updateCommentDetail = async (commentId: number) => {
     const updateReply = {
       updateData: {
-        content: editComment,
-        anon,
-        secret
+        content: editComment
       },
       commentId
     };
-    updateMutation.mutate(updateReply);
+    updateMutation.mutate(updateReply, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['comments', paramId]);
+        setAnon(false);
+        setComment('');
+        setSecret(false);
+        setIsEdit(false);
+        setEditedCommentIndex(null);
+      }
+    });
   };
-  const deleteMutation = useMutation(deleteCommentMutation, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('comments');
-      setEditedCommentIndex(null);
-    }
-  });
+
   const deleteComment = async (commentId: number) => {
     if (window.confirm('해당 댓글을 삭제하시겠습니까?')) {
-      deleteMutation.mutate(commentId);
+      deleteMutation.mutate(commentId, {
+        onSuccess: () => {
+          queryClient.invalidateQueries('comments');
+          setEditedCommentIndex(null);
+        }
+      });
     } else {
       alert('삭제취소');
     }
   };
   const handleReplyClick = (comment: ReplyObject) => {
-    setIsReply(true); // 대댓글 입력 활성화
-    setReplyingToComment(comment); // 대댓글 대상 댓글 정보 저장
+    setReplyingToComment(comment);
     setParentId(comment.id);
     setParentUserId(comment.user_id);
   };
@@ -243,120 +234,39 @@ const Reply: React.FC<CommentProps> = ({
         <St.CommentIcon className="comment" />
         <p>{`${commentUserInfo?.length}개의 댓글`}</p>
       </St.CountDivTop>
+      <ReplyForm
+        addComment={addComment}
+        isFocused={isFocused}
+        commentFocusHandler={commentFocusHandler}
+        setIsFocused={setIsFocused}
+        comment={comment}
+        setComment={setComment}
+        replyingToComment={replyingToComment}
+        anon={anon}
+        setAnon={setAnon}
+        secret={secret}
+        setSecret={setSecret}
+      />
 
-      <St.Form
-        onSubmit={addComment}
-        $isFocused={isFocused}
-        onFocus={commentFocusHandler}
-        onBlur={() => setIsFocused(false)}
-      >
-        <div>
-          <St.CommentInput
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            placeholder={
-              replyingToComment
-                ? replyingToComment.anon
-                  ? '익명의 작업자에게 답글 달기...'
-                  : `@${
-                      replyingToComment.user.nickname ??
-                      replyingToComment.user.username
-                    }에게 답글 달기...`
-                : '댓글을 입력하세요'
-            }
-          />
-          <button type="submit">
-            <St.SendIcon />
-          </button>
-        </div>
-
-        <St.CheckBoxArea>
-          <St.AnonLabel>
-            <St.CheckBoxs
-              type="checkbox"
-              checked={anon}
-              onChange={() => setAnon(!anon)}
-            />
-            익명
-          </St.AnonLabel>
-          <St.AnonLabel>
-            <St.CheckBoxs
-              type="checkbox"
-              checked={secret}
-              onChange={() => setSecret(!secret)}
-            />
-            비밀
-          </St.AnonLabel>
-        </St.CheckBoxArea>
-      </St.Form>
-
-      {commentUserInfo
-        ?.filter((comment) => {
-          return comment.parent_id === null;
-        })
-        .map((comment, index) => {
-          const parseTime = parseDate(comment.created_at);
-          const canViewSecretComment =
-            !comment.secret ||
-            comment.user_id === userId || // 현재 사용자가 댓글 작성자
-            postUserId === userId ||
-            comment.parentUser_id === userId;
-          return (
-            <St.CommentContainer key={index}>
-              <St.Comment>
-                {comment.secret && !canViewSecretComment ? (
-                  <St.SecretComment>이 댓글은 비밀댓글입니다.</St.SecretComment>
-                ) : (
-                  <St.ParentComment>
-                    <ReplyContent
-                      comment={comment}
-                      profile={profile}
-                      isEdit={isEdit}
-                      editComment={editComment}
-                      editedCommentIndex={editedCommentIndex}
-                      startEditComment={startEditComment}
-                      updateCommentDetail={updateCommentDetail}
-                      deleteComment={deleteComment}
-                      setEditComment={setEditComment}
-                    />
-                    <button onClick={() => handleReplyClick(comment)}>
-                      대댓글
-                    </button>
-                  </St.ParentComment>
-                )}
-
-                {childComment(comment.id)
-                  ?.reverse()
-                  .map((comment, index) => (
-                    <St.ChildCommentContainer key={index}>
-                      <p>ㄴ</p>
-                      {comment.secret && !canViewSecretComment ? (
-                        <St.CommentContent>
-                          이 댓글은 비밀댓글입니다.
-                        </St.CommentContent>
-                      ) : (
-                        <St.ChildComment>
-                          <ReplyContent
-                            comment={comment}
-                            profile={profile}
-                            isEdit={isEdit}
-                            editComment={editComment}
-                            editedCommentIndex={editedCommentIndex}
-                            startEditComment={startEditComment}
-                            updateCommentDetail={updateCommentDetail}
-                            deleteComment={deleteComment}
-                            setEditComment={setEditComment}
-                          />
-                        </St.ChildComment>
-                      )}
-                    </St.ChildCommentContainer>
-                  ))}
-              </St.Comment>
-
-              {/* <button>신고</button> */}
-            </St.CommentContainer>
-          );
-        })}
+      <ReplyList
+        comments={commentUserInfo || []}
+        profile={profile}
+        isEdit={isEdit}
+        editComment={editComment}
+        editedCommentIndex={editedCommentIndex}
+        handleReplyClick={handleReplyClick}
+        startEditComment={startEditComment}
+        updateCommentDetail={updateCommentDetail}
+        deleteComment={deleteComment}
+        setEditComment={setEditComment}
+        canViewSecretComment={(comment: ReplyObject) =>
+          !comment.secret ||
+          comment.user_id === userId ||
+          postUserId === userId ||
+          comment.parentUser_id === userId
+        }
+        childComment={childComment}
+      />
     </St.Container>
   );
 };
